b8f2d37d5ec455594862dce6d8f2420f2952f8a5
Jericho Sandoval <86719994+ekyut123@users.noreply.github.com>
2022-12-06 20:08:09 +0800

    Update admin.dart

--- lib/admin.dart
+++ lib/admin.dart
@@ -14,50 +14,84 @@ class _AdminPageState extends State<AdminPage> {
     FirebaseAuth.instance.signOut();
   }
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: <Widget>[
+//           const Text(
+//             "Welcome! Admin",
+//             style: TextStyle(
+//               color: Colors.black,
+//               fontWeight: FontWeight.bold,
+//               fontFamily: 'Montserrat',
+//               fontSize: 30,
+//             ),
+//           ),
+//           const SizedBox(
+//             height: 30,
+//           ),
+//           SizedBox(
+//             height: 40,
+//             child: Material(
+//               borderRadius: BorderRadius.circular(20),
+//               shadowColor: Colors.greenAccent,
+//               color: Colors.black,
+//               elevation: 7,
+//               child: GestureDetector(
+//                 onTap: () async {
+//                   _signOut();
+//                 },
+//                 child: const Center(
+//                   child: Text(
+//                     'LOGOUT',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontWeight: FontWeight.bold,
+//                       fontFamily: 'Montserrat',
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+  List<String> data = [
+    "#1\nName\nService",
+    "#2\nName\nService",
+    "#3\nName\nService",
+    "#4\nName\nService",
+    "#5\nName\nService",
+    "#6\nName\nService",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text(
-            "Welcome! Admin",
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Montserrat',
-              fontSize: 30,
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-            height: 40,
-            child: Material(
-              borderRadius: BorderRadius.circular(20),
-              shadowColor: Colors.greenAccent,
-              color: Colors.black,
-              elevation: 7,
-              child: GestureDetector(
-                onTap: () async {
-                  _signOut();
-                },
-                child: const Center(
-                  child: Text(
-                    'LOGOUT',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Montserrat',
-                    ),
+      appBar: AppBar(
+        title: const Text("Admin"),
+      ),
+      body: ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            return Card(
+              color: Colors.deepOrange,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ListTile(
+                  title: Text(data[index]),
+                  trailing: const SizedBox(
+                    width: 100,
                   ),
                 ),
               ),
-            ),
-          ),
-        ],
-      ),
+            );
+          }),
     );
   }
 }
