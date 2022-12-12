2275d95c7e1a59df95a546c70e0ab4ed7df55533
Jericho Sandoval <86719994+ekyut123@users.noreply.github.com>
2022-12-12 15:29:07 +0800

    Update business_model.dart

--- lib/model/business_model.dart
+++ lib/model/business_model.dart
@@ -1,46 +1,56 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class BusinessInfo {
-  final String id;
+  final String businessid;
   final String businessName;
   final String businessDescription;
   final String businessType;
   final String businessImage;
+  final String openingday;
+  final String openinghour;
+  final String closingday;
+  final String closinghour;
+  final String businessaddress;
+  // final CategoryInfo category;
 
   const BusinessInfo(
-      {required this.id,
+      {required this.businessid,
       required this.businessName,
       required this.businessDescription,
       required this.businessType,
-      required this.businessImage});
+      required this.businessImage,
+      required this.openingday,
+      required this.openinghour,
+      required this.closingday,
+      required this.closinghour,
+      required this.businessaddress});
 
   factory BusinessInfo.fromSnapshot(DocumentSnapshot snap) {
     var snapshot = snap.data() as Map<String, dynamic>;
 
     return BusinessInfo(
-        id: snapshot['id'],
+        businessid: snapshot['businessid'],
         businessName: snapshot['businessName'],
         businessDescription: snapshot['businessDescription'],
         businessType: snapshot['businessType'],
-        businessImage: snapshot['businessImage']);
+        businessImage: snapshot['businessImage'],
+        openingday: snapshot['opening day'],
+        openinghour: snapshot['opening hour'],
+        closingday: snapshot['closing day'],
+        closinghour: snapshot['closing hour'],
+        businessaddress: snapshot['business address']);
   }
 
   Map<String, dynamic> toJson() => {
-        "id": id,
+        "businessid": businessid,
         "businessName": businessName,
         "businessDescription": businessDescription,
         "businessType": businessType,
-        "businessImage": businessImage
+        "businessImage": businessImage,
+        "opening day": openingday,
+        "opening hour": openinghour,
+        "closing day": closingday,
+        "closing hour": closinghour,
+        "business address": businessaddress
       };
-
-  // static BusinessInfo fromSnapshot(DocumentSnapshot snap){
-  //   BusinessInfo businessinfo = BusinessInfo(
-  //     id: snap['id'],
-  //     businessName: snap['businessName'],
-  //     businessDescription: snap['businessDescription'],
-  //     businessType: snap['businessType'],
-  //     businessImage: snap['businessImage']
-  //   );
-  //   return businessinfo;
-  // }
 }
