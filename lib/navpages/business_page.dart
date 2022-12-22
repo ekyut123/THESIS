0871aaf691b26880fa4032ed378d82ff2f38501f
unikaoba <89960942+unikaoba@users.noreply.github.com>
2022-12-22 15:39:59 +0800

    Update business_page.dart

--- lib/navpages/business_page.dart
+++ lib/navpages/business_page.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import '../widgets/app_large_text.dart';
 import '../widgets/app_short_text.dart';
 import '../widgets/navigation_drawer.dart';
@@ -7,83 +8,139 @@ import 'category_list.dart';
 
 class BusinessPage extends StatelessWidget {
   final String chosenbusiness;
-  final String name;
-  final String openingday;
-  final String openinghour;
-  final String closingday;
-  final String closinghour;
-  final String businessaddress;
-  final String businessimage;
 
-  const BusinessPage(
-      {super.key,
-      required this.chosenbusiness,
-      required this.name,
-      required this.openingday,
-      required this.openinghour,
-      required this.closingday,
-      required this.closinghour,
-      required this.businessaddress,
-      required this.businessimage});
+  const BusinessPage({
+    super.key,
+    required this.chosenbusiness,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const NavigationDrawerWidget(),
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.deepOrange,
-        title: Text(name),
-        //MODIFIED FROM HERE
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.feedback_rounded))
-        ],
-        // UNTIL HERE
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.2,
-            decoration: BoxDecoration(color: Colors.grey[100]),
-          ),
-          Row(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.7,
-                padding: const EdgeInsets.only(left: 15, top: 10),
-                alignment: Alignment.topLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    AppLargeText(text: name),
-                    const SizedBox(height: 5),
-                    AppShortText(text: businessaddress, color: Colors.black)
-                  ],
-                ),
-              ),
-              Container(
-                  width: MediaQuery.of(context).size.width * 0.3,
-                  padding: const EdgeInsets.only(right: 15, top: 20),
-                  alignment: Alignment.topRight,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text("${openingday} - ${closingday}"),
-                      Text("${openinghour} - ${closinghour}"),
+
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    String email = "";
+    String firstName = "";
+    String lastName = "";
+    String phoneNumber = "";
+
+    Future<DocumentSnapshot<Map<String, dynamic>>> getUser() async {
+    User? user = _auth.currentUser!;
+    return await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(user.uid)
+        .get();
+  }
+
+    String businessaddress = "";
+    String businessName = "";
+    String closingday = "";
+    String closinghour = "";
+    String openingday = "";
+    String openinghour = "";
+    String businessImage = "";
+
+    Future<DocumentSnapshot<Map<String, dynamic>>> readbusinessinfo(String businessid) async{
+      return await FirebaseFirestore.instance.collection('BusinessList').doc(businessid).get();
+    }
+
+    return FutureBuilder(
+      future: getUser(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return const Center(child: Text('some error occured'));
+        }
+        if(snapshot.hasData){
+          email = snapshot.data!['email'];
+          firstName = snapshot.data!['first name'];
+          lastName = snapshot.data!['last name'];
+          phoneNumber = snapshot.data!['phone number'];
+
+          return FutureBuilder(
+            future: readbusinessinfo(chosenbusiness),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return const Center(child: Text('some error occured'));
+              }
+              if(snapshot.hasData){
+                businessImage = snapshot.data!['businessImage'];
+                businessaddress = snapshot.data!['business address'];
+                businessName = snapshot.data!['businessName'];
+                closingday = snapshot.data!['closing day'];
+                closinghour = snapshot.data!['closing hour'];
+                openingday = snapshot.data!['opening day'];
+                openinghour = snapshot.data!['opening hour'];
+                
+                return Scaffold(
+                  drawer: NavigationDrawerWidget(
+                    consumeremail: email,
+                    consumerfirstname: firstName,
+                    consumerlastname: lastName,
+                    consumerphonenum: phoneNumber
+                  ),
+                  appBar: AppBar(
+                    centerTitle: true,
+                    backgroundColor: Colors.deepOrange,
+                    title: Text(businessName),
+                    actions: [
+                      IconButton(onPressed: () {}, icon: const Icon(Icons.feedback_rounded))
                     ],
-                  )),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Expanded(
-            child: ShowCategoryList(
-              chosenbusinessid: chosenbusiness,
-              chosenbusinessname: name,
-              chosenbusinessloc: businessaddress,
-            ),
-          ),
-        ],
-      ),
+                  ),
+                  body: Column(
+                  children: [
+                    Container(
+                          height: MediaQuery.of(context).size.height * 0.2,
+                          decoration: BoxDecoration(color: Colors.grey[100]),
+                        ),
+                    Row(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.7,
+                              padding: const EdgeInsets.only(left: 15, top: 10),
+                              alignment: Alignment.topLeft,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  AppLargeText(text: businessName),
+                                  const SizedBox(height: 5),
+                                  AppShortText(text: businessaddress, color: Colors.black)
+                                ],
+                              ),
+                            ),
+                            Container(
+                                width: MediaQuery.of(context).size.width * 0.3,
+                                padding: const EdgeInsets.only(right: 15, top: 20),
+                                alignment: Alignment.topRight,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Text("${openingday} - ${closingday}"),
+                                    Text("${openinghour} - ${closinghour}"),
+                                  ],
+                                )),
+                          ],
+                        ),
+                    const SizedBox(height: 10),
+                    Expanded(
+                          child: ShowCategoryList(
+                            chosenbusinessid: chosenbusiness,
+                          ),
+                        ),
+                      ],
+                  )
+                );
+              }
+              return const Center(child: CircularProgressIndicator());
+            }
+          );
+        }
+        return const Center(child: CircularProgressIndicator());
+      }
     );
   }
 }
