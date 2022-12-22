6e4069311ba127f45e32e6f32a666d56e1bbeb0f
unikaoba <89960942+unikaoba@users.noreply.github.com>
2022-12-22 15:39:29 +0800

    Update active_booking.dart

--- lib/navpages/active_booking.dart
+++ lib/navpages/active_booking.dart
@@ -1,138 +1,161 @@
-// // ignore_for_file: unnecessary_new
-// import 'package:flutter/material.dart';
-// import 'package:flutter_firebase_users/navpages/booking_page.dart';
-// import 'package:flutter_firebase_users/data_source/booking_DB.dart';
-// import 'package:flutter_firebase_users/model/booking_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_users/data_source/read_DB.dart';
+import '../model/active_booking.dart';
+import 'booking_page.dart';
+import 'package:intl/intl.dart';
 
-// // ignore: camel_case_types, must_be_immutable
-// class ActiveBooking extends StatefulWidget {
-//   const ActiveBooking({Key? key}) : super(key: key);
+class ActiveBookingPage extends StatefulWidget {
+  const ActiveBookingPage({super.key});
 
-//   @override
-//   State<ActiveBooking> createState() => _ActiveBooking();
-// }
+  @override
+  State<ActiveBookingPage> createState() => _ActiveBookingPageState();
+}
 
-// class _ActiveBooking extends State<ActiveBooking> {
-//   void showAlertDialog(context, index) {
-//     // set up the buttons
-//     Widget cancelButton = TextButton(
-//       style: TextButton.styleFrom(
-//         backgroundColor: Colors.deepOrange,
-//       ),
-//       child: const Text(
-//         "Yes",
-//         style: TextStyle(color: Colors.white),
-//       ),
-//       onPressed: () {
-//         Navigator.of(context).pop();
-//         setState(() {
-//            //booking.removeAt(index);
-//         });
-//       },
-//     );
-//     Widget continueButton = TextButton(
-//       style: TextButton.styleFrom(
-//         backgroundColor: Colors.deepOrange,
-//       ),
-//       child: const Text(
-//         "No",
-//         style: TextStyle(color: Colors.white),
-//       ),
-//       onPressed: () {
-//         Navigator.of(context).pop();
-//       },
-//     );
+class _ActiveBookingPageState extends State<ActiveBookingPage> {
 
-//     // set up the AlertDialog
-//     AlertDialog alert = AlertDialog(
-//       title: const Text(
-//         "Cancellation",
-//         style: TextStyle(color: Colors.deepOrange),
-//       ),
-//       content: const Text("Are you sure that you want to cancel your booking?"),
-//       actions: [
-//         cancelButton,
-//         continueButton,
-//       ],
-//     );
+  @override
+  Widget build(BuildContext context) {
 
-//     // show the dialog
-//     showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return alert;
-//       },
-//     );
-//   }
+    void showAlertDialog(context, index) {
+    // set up the buttons
+    Widget cancelButton = TextButton(
+      style: TextButton.styleFrom(
+        backgroundColor: Colors.deepOrange,
+      ),
+      child: const Text(
+        "Yes",
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {
+        Navigator.of(context).pop();
+        setState(() {
+           //booking.removeAt(index);
+        });
+      },
+    );
+    
+    Widget continueButton = TextButton(
+      style: TextButton.styleFrom(
+        backgroundColor: Colors.deepOrange,
+      ),
+      child: const Text(
+        "No",
+        style: TextStyle(color: Colors.white),
+      ),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return StreamBuilder<List<BookingInfo>>(
-//         stream: BookingDB.readbookinginfo(),
-//         builder: (context, snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             return const Center(child: CircularProgressIndicator());
-//           }
-//           if (snapshot.hasError) {
-//             return const Center(child: Text('some error occured'));
-//           }
-//           if (snapshot.hasData) {
-//             final booking = snapshot.data;
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: const Text(
+        "Cancellation",
+        style: TextStyle(color: Colors.deepOrange),
+      ),
+      content: const Text("Are you sure that you want to cancel your booking?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
 
-//             return Scaffold(
-//               appBar: AppBar(
-//                 title: const Text("Active Booking"),
-//               ),
-//               body: ListView.builder(
-//                   itemCount: booking!.length,
-//                   itemBuilder: (context, index) {
-//                     return Card(
-//                       color: Colors.deepOrange,
-//                       child: ListTile(
-//                         title: Text(booking[index].date),
-//                         trailing: Row(
-//                           mainAxisSize: MainAxisSize.min,
-//                           children: [
-//                             ElevatedButton(
-//                                 onPressed: () {
-//                                   showAlertDialog(context, index);
-//                                 },
-//                                 style: ElevatedButton.styleFrom(
-//                                     backgroundColor: Colors
-//                                         .white, //change for background color of the button
-//                                     foregroundColor: Colors.black),
-//                                 child: const Text(
-//                                     'Cancel') //change for the text color of button
-//                                 ),
-//                             const SizedBox(width: 8),
-//                             ElevatedButton(
-//                                 onPressed: () {
-//                                   Navigator.push(
-//                                       context,
-//                                       MaterialPageRoute(
-//                                           builder: (BuildContext context) =>
-//                                               const BookingPage(
-//                                                 name: '',
-//                                                 serviceDescription: '',
-//                                                 serviceimage: '',
-//                                                 serviceprice: '',
-//                                               )));
-//                                 },
-//                                 style: ElevatedButton.styleFrom(
-//                                     backgroundColor: Colors
-//                                         .white, //change for background color of the button
-//                                     foregroundColor: Colors.black),
-//                                 child: const Text(
-//                                     'Reschedule') //change for the text color of button
-//                                 ),
-//                           ],
-//                         ),
-//                       ),
-//                     );
-//                   }),
-//             );
-//           }
-//           return const Center(child: CircularProgressIndicator());
-//         });
-//   }
-// }
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+    final String userid = FirebaseAuth.instance.currentUser!.uid;
+    return StreamBuilder<List<ActiveBooking>>(
+      stream: ReadDataBase.readactivebooking(userid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          print(snapshot.error);
+          return const Center(child: Text('some error occured'));
+        }
+        if (snapshot.hasData){
+          final activebooking = snapshot.data;
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text("Active Booking"),
+              ),
+              body: ListView.builder(
+                  itemCount: activebooking!.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      color: Colors.deepOrange,
+                      child: ListTile(
+                        title: Text(activebooking[index].servicename),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            //cancel
+                            ElevatedButton(
+                              onPressed: () {
+                                showAlertDialog(context, index);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors                
+                                .white, //change for background color of the button
+                                foregroundColor: Colors.black),
+                                child: const Text('Cancel')),
+                            const SizedBox(width: 8),
+                            //resched
+                            ElevatedButton(
+                                onPressed: () {
+                                  final dt = DateTime.now();
+                                  final int DV = 0;
+                                  //day
+                                  final String dy = DateFormat.d().format(dt);
+                                  int intdy = int.tryParse(dy) ?? DV;
+                                  //month
+                                  final String month = DateFormat.MMM().format(dt);
+                                  final String nummonth = DateFormat.M().format(dt);
+                                  final int intmonth = int.tryParse(nummonth) ?? DV;
+                                  //year
+                                  final String yr = DateFormat.y().format(dt);
+                                  int intyr = int.tryParse(yr) ?? DV;
+                                  //get date tom
+                                  int intdytom = intdy + 1;
+                                  final String daytom = intdytom.toString();
+                                  final String date = "${daytom} ${month}, ${yr}";
+
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (BuildContext context) =>
+                                               BookingPage(
+                                                chosenserviceid: activebooking[index].serviceid,
+                                                date: date,
+                                                intdy: intdytom,
+                                                intmonth: intmonth,
+                                                intyr: intyr
+                                              )));
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors
+                                        .white, //change for background color of the button
+                                    foregroundColor: Colors.black),
+                                child: const Text(
+                                    'Reschedule') //change for the text color of button
+                                ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+            );
+        }
+        return const Center(child: CircularProgressIndicator());
+
+      },
+    );
+  }
+}
