c64bc567395ee707133742a2e5a32ed5de56aa79
Jericho Sandoval <86719994+ekyut123@users.noreply.github.com>
2022-12-24 16:35:38 +0800

    Update navigation_drawer.dart

--- lib/widgets/navigation_drawer.dart
+++ lib/widgets/navigation_drawer.dart
@@ -1,181 +1,172 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_users/navpages/booking_history.dart';
+import '../navpages/active_booking.dart';
 import '../navpages/contact-us_page.dart';
 import '../consumer.dart';
-import '../navpages/active_booking.dart';
 import '../navpages/settings_page.dart';
 import '../navpages/termsandconditions_page.dart';
 
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  String email = "";
-  String firstName = "";
-  String lastName = "";
-  String phoneNumber = "";
-  String uid = "";
+final FirebaseAuth _auth = FirebaseAuth.instance;
+String email = "";
+String firstName = "";
+String lastName = "";
+String phoneNumber = "";
+String uid = "";
 
 class NavigationDrawerWidget extends StatelessWidget {
-  
-  const NavigationDrawerWidget({
-    super.key
-  });
+  const NavigationDrawerWidget({super.key});
   Future<void> _signOut() async {
     FirebaseAuth.instance.signOut();
   }
 
   @override
   Widget build(BuildContext context) {
-
     Future<DocumentSnapshot<Map<String, dynamic>>> getUser() async {
-    User? user = _auth.currentUser!;
-    return await FirebaseFirestore.instance
-        .collection('Users')
-        .doc(user.uid)
-        .get();
-  }
-    return FutureBuilder(
-      future: getUser(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        if (snapshot.hasError) {
-          return const Center(child: Text('some error occured'));
-        }
-        if(snapshot.hasData){
-          email = snapshot.data!['email'];
-          firstName = snapshot.data!['first name'];
-          uid = snapshot.data!['id'];
-          lastName = snapshot.data!['last name'];
-          phoneNumber = snapshot.data!['phone number'];
+      User? user = _auth.currentUser!;
+      return await FirebaseFirestore.instance
+          .collection('Users')
+          .doc(user.uid)
+          .get();
+    }
 
-          return Drawer(
-            child: Material(
-              color: Colors.deepOrange,
-              child: ListView(
-                children: <Widget>[
-                  const SizedBox(height: 10.0),
-                  //home
-                  buildMenuItem(
-                    text: 'Home',
-                    icon: Icons.article_outlined,
-                    onClicked: (() {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) => const ConsumerPage(),
-                        )
-                      );
-                    })
-                  ),
-                  const SizedBox(height: 10.0),
-                  // terms and condition
-                  // buildMenuItem(
-                  //   text: 'Active Booking',
-                  //   icon: Icons.article_outlined,
-                  //   onClicked: (() {
-                  //     Navigator.push(
-                  //       context,
-                  //       MaterialPageRoute(
-                  //         builder: (BuildContext context) => const ActiveBookingPage(),
-                  //       )
-                  //     );
-                  //   })
-                  // ),
-                  const SizedBox(height: 10.0),
-                  //booking history
-                  // buildMenuItem(
-                  //   text: 'Booking History',
-                  //   icon: Icons.history,
-                  //   onClicked: (() {
-                  //     Navigator.push(
-                  //       context,
-                  //       MaterialPageRoute(
-                  //         builder: (BuildContext context) => const BookingHistory(),
-                  //       )
-                  //     );
-                  //   })
-                  // ),
-                  const SizedBox(height: 10.0),
-                  //fave
-                  buildMenuItem(
-                    text: 'Favorites',
-                    icon: Icons.favorite_border,
-                  ),
-                  const SizedBox(height: 10.0),
-                  //contact us
-                  buildMenuItem(
-                    text: 'Contact Us',
-                    icon: Icons.contacts,
-                    onClicked: (() {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) => const ContactUs(),
-                        )
-                      );
-                    })
-                  ),
-                  const SizedBox(height: 10.0),
-                  //terms and condition
-                  buildMenuItem(
-                    text: 'Terms & Conditions',
-                    icon: Icons.article_outlined,
-                    onClicked: (() {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) => TermsAndConditions(),
-                        )
-                      );
-                    })
-                  ),
-                  const SizedBox(height: 10.0),
-                  buildMenuItem(
-                    text: 'Settings',
-                    icon: Icons.settings,
-                    onClicked: (() {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (BuildContext context) => const SettingsPage(),
-                        )
-                      );
-                    })
-                  ),
-                  const SizedBox(height: 270.0),
-                  //added for logout -rob :)
-                  Row(
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(20, 20, 30, 20),
-                        child: Icon(
-                          Icons.logout,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(
-                        child: GestureDetector(
-                          onTap: _signOut,
-                          child: const Text(
-                            "Logout",
-                            style: TextStyle(
-                                color: Colors.white, fontWeight: FontWeight.bold),
+    return FutureBuilder(
+        future: getUser(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return const Center(child: Text('some error occured'));
+          }
+          if (snapshot.hasData) {
+            email = snapshot.data!['email'];
+            firstName = snapshot.data!['first name'];
+            uid = snapshot.data!['id'];
+            lastName = snapshot.data!['last name'];
+            phoneNumber = snapshot.data!['phone number'];
+
+            return Drawer(
+              child: Material(
+                color: Colors.deepOrange,
+                child: ListView(
+                  children: <Widget>[
+                    const SizedBox(height: 10.0),
+                    //home
+                    buildMenuItem(
+                        text: 'Home',
+                        icon: Icons.article_outlined,
+                        onClicked: (() {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    const ConsumerPage(),
+                              ));
+                        })),
+                    const SizedBox(height: 10.0),
+                    // terms and condition
+                    buildMenuItem(
+                        text: 'Active Booking',
+                        icon: Icons.article_outlined,
+                        onClicked: (() {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    const ActiveBookingPage(),
+                              ));
+                        })),
+                    const SizedBox(height: 10.0),
+                    //booking history
+                    buildMenuItem(
+                        text: 'Booking History',
+                        icon: Icons.history,
+                        onClicked: (() {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    const BookingHistoryPage(),
+                              ));
+                        })),
+                    const SizedBox(height: 10.0),
+                    //fave
+                    buildMenuItem(
+                      text: 'Favorites',
+                      icon: Icons.favorite_border,
+                    ),
+                    const SizedBox(height: 10.0),
+                    //contact us
+                    buildMenuItem(
+                        text: 'Contact Us',
+                        icon: Icons.contacts,
+                        onClicked: (() {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    const ContactUs(),
+                              ));
+                        })),
+                    const SizedBox(height: 10.0),
+                    //terms and condition
+                    buildMenuItem(
+                        text: 'Terms & Conditions',
+                        icon: Icons.article_outlined,
+                        onClicked: (() {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    TermsAndConditions(),
+                              ));
+                        })),
+                    const SizedBox(height: 10.0),
+                    buildMenuItem(
+                        text: 'Settings',
+                        icon: Icons.settings,
+                        onClicked: (() {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    const SettingsPage(),
+                              ));
+                        })),
+                    const SizedBox(height: 270.0),
+                    //added for logout -rob :)
+                    Row(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(20, 20, 30, 20),
+                          child: Icon(
+                            Icons.logout,
+                            color: Colors.white,
                           ),
                         ),
-                      )
-                    ],
-                  )
-                ],
+                        SizedBox(
+                          child: GestureDetector(
+                            onTap: _signOut,
+                            child: const Text(
+                              "Logout",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          );
-        }
-        return const Center(child: CircularProgressIndicator());
-      }
-    );
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        });
   }
 
   Widget buildMenuItem({
