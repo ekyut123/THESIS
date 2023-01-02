6b79bd66157dc0e4990b6f39ca036931e7b04ca8
robnnj <103233129+robnnj@users.noreply.github.com>
2023-01-02 16:40:08 +0800

    Update logoutAdmin.dart

--- lib/admin/logoutAdmin.dart
+++ lib/admin/logoutAdmin.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 
-GlobalKey<FormState> _formKey3 = GlobalKey<FormState>();
-
 class LogoutAfter extends StatefulWidget {
   const LogoutAfter({super.key});
 
@@ -20,69 +18,66 @@ class _LogoutAfterState extends State<LogoutAfter> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: _formKey3,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Stack(
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.fromLTRB(15, 110, 0, 0),
-                  child: const Text(
-                    "Click logout and log back in with your new password",
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.fromLTRB(15, 110, 0, 0),
+                child: const Text(
+                  "Click logout and log back in with your new password",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Center(
+            child: SizedBox(
+              width: 200,
               height: 40,
-            ),
-            Center(
-              child: SizedBox(
-                width: 200,
-                height: 40,
-                child: GestureDetector(
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          content: const Text('Please re-log to your account',
-                              style: TextStyle(color: Colors.black)),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                _signOut();
-                                Navigator.of(context).pushNamedAndRemoveUntil(
-                                    '/main', (Route<dynamic> route) => false);
-                              },
-                              child: const Text(
-                                'Logout',
-                                style: TextStyle(color: Colors.blue),
-                                textAlign: TextAlign.center,
-                              ),
+              child: GestureDetector(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        content: const Text('Please re-log to your account',
+                            style: TextStyle(color: Colors.black)),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              _signOut();
+                              Navigator.of(context).pushNamedAndRemoveUntil(
+                                  '/main', (Route<dynamic> route) => false);
+                            },
+                            child: const Text(
+                              'Logout',
+                              style: TextStyle(color: Colors.blue),
+                              textAlign: TextAlign.center,
                             ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  child: const Material(
-                    shadowColor: Colors.greenAccent,
-                    color: Colors.black,
-                    elevation: 7,
-                    child: MouseRegion(
-                      cursor: SystemMouseCursors.click,
-                      child: Center(
-                        child: Text(
-                          'Logout',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'Montserrat',
                           ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Material(
+                  shadowColor: Colors.greenAccent,
+                  color: Colors.black,
+                  elevation: 7,
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: Center(
+                      child: Text(
+                        'Logout',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat',
                         ),
                       ),
                     ),
@@ -90,8 +85,8 @@ class _LogoutAfterState extends State<LogoutAfter> {
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
