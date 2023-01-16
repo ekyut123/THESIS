1c6ee62f8db5b6d06e33ddb4a1f0853a319f78bb
unikaoba <89960942+unikaoba@users.noreply.github.com>
2023-01-16 20:31:38 +0800

    Update personalcare_list.dart

--- lib/navpages/personalcare_list.dart
+++ lib/navpages/personalcare_list.dart
@@ -1,63 +1,72 @@
 import 'package:flutter/material.dart';
+import '../model/counter_model.dart';
 import '../data_source/read_DB.dart';
-import '../model/business_model.dart';
 import 'business_page.dart';
 
-class PersonalCareList extends StatelessWidget {
+class PersonalCareList extends StatefulWidget {
   final String label;
+  final String uid;
   const PersonalCareList({
     super.key,
     required this.label,
+    required this.uid
   });
 
   @override
+  State<PersonalCareList> createState() => _PersonalCareListState();
+}
+
+class _PersonalCareListState extends State<PersonalCareList> {
+  
+  @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<BusinessInfo>>(
-      stream: ReadDataBase.readpersonalcareinfo(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        if (snapshot.hasError) {
-          return const Center(child: Text('some error occured'));
-        }
-        if (snapshot.hasData) {
-          final personalcareData = snapshot.data;
-          return ListView.builder(
-            itemCount: personalcareData!.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  debugPrint(personalcareData[index].businessid);
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) => BusinessPage(
-                                chosenbusiness:
-                                personalcareData[index].businessid
+        return StreamBuilder<List<CounterModel>>(
+          stream: ReadDataBase.readbookedcounter(widget.uid, widget.label),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              print(snapshot.error);
+              return const Center(child: Text('some error occured'));
+            }
+            if (snapshot.hasData) {
+              final personalcareData = snapshot.data;
+              return ListView.builder(
+                itemCount: personalcareData!.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      debugPrint(personalcareData[index].businessid);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => BusinessPage(
+                                    chosenbusiness:
+                                    personalcareData[index].businessid
+                              )
                           )
-                      )
-                  );
-                },
-                child: Container(
-                  margin: const EdgeInsets.symmetric(vertical: 5),
-                  child: ListTile(
-                    //businessimage
-                    leading: CircleAvatar(
-                      radius: 30,
-                      backgroundImage:
-                        NetworkImage(personalcareData[index].businessImage),
-                    ),
-                    //businessname
-                    title: Text(personalcareData[index].businessName),
-                    subtitle: Text(personalcareData[index].businessDescription)),
-                  ),
+                      );
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(vertical: 5),
+                      child: ListTile(
+                        //businessimage
+                        leading: CircleAvatar(
+                          radius: 30,
+                          backgroundImage:
+                            NetworkImage(personalcareData[index].businessImage),
+                        ),
+                        //businessname
+                        title: Text(personalcareData[index].businessName),
+                        subtitle: Text(personalcareData[index].businessDescription)),
+                      ),
+                    );
+                  },
                 );
-              },
-            );
-        }
-        return const Center(child: CircularProgressIndicator());
-      }
-    );
+            }
+            return const Center(child: CircularProgressIndicator());
+          }
+        );
   }
 }
