99f45cbea70db15e9751a12a5fe1e5a13869ba1a
unikaoba <89960942+unikaoba@users.noreply.github.com>
2023-01-16 20:32:13 +0800

    Update healthcare_list.dart

--- lib/navpages/healthcare_list.dart
+++ lib/navpages/healthcare_list.dart
@@ -1,31 +1,38 @@
 import 'package:flutter/material.dart';
+import '../model/counter_model.dart';
 import '../data_source/read_DB.dart';
-import '../model/business_model.dart';
 import 'business_page.dart';
 
-class HealthCareList extends StatelessWidget {
+class HealthCareList extends StatefulWidget {
   final String label;
-
+  final String uid;
+  
   const HealthCareList({
     super.key,
     required this.label,
+    required this.uid
   });
 
   @override
+  State<HealthCareList> createState() => _HealthCareListState();
+}
+
+class _HealthCareListState extends State<HealthCareList> {
+  
+  @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<BusinessInfo>>(
-        stream: ReadDataBase.readhealthcareinfo(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return const Center(child: Text('some error occured'));
-          }
-          if (snapshot.hasData) {
-            final healthcareData = snapshot.data;
-            return Expanded(
-              child: ListView.builder(
+        return StreamBuilder<List<CounterModel>>(
+          stream: ReadDataBase.readbookedcounter(widget.uid, widget.label),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return const Center(child: Text('some error occured'));
+            }
+            if (snapshot.hasData) {
+              final healthcareData = snapshot.data;
+              return ListView.builder(
                 itemCount: healthcareData!.length,
                 itemBuilder: (context, index) {
                   return GestureDetector(
@@ -35,8 +42,11 @@ class HealthCareList extends StatelessWidget {
                           context,
                           MaterialPageRoute(
                               builder: (BuildContext context) => BusinessPage(
-                                  chosenbusiness:
-                                      healthcareData[index].businessid)));
+                                    chosenbusiness:
+                                    healthcareData[index].businessid
+                              )
+                          )
+                      );
                     },
                     child: Container(
                       margin: const EdgeInsets.symmetric(vertical: 5),
@@ -45,20 +55,18 @@ class HealthCareList extends StatelessWidget {
                         leading: CircleAvatar(
                           radius: 30,
                           backgroundImage:
-                              NetworkImage(healthcareData[index].businessImage),
+                            NetworkImage(healthcareData[index].businessImage),
                         ),
                         //businessname
                         title: Text(healthcareData[index].businessName),
-                        subtitle:
-                            Text(healthcareData[index].businessDescription),
+                        subtitle: Text(healthcareData[index].businessDescription)),
                       ),
-                    ),
-                  );
-                },
-              ),
-            );
+                    );
+                  },
+                );
+            }
+            return const Center(child: CircularProgressIndicator());
           }
-          return const Center(child: CircularProgressIndicator());
-        });
+        );
   }
 }
