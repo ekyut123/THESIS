563dc90721af83ef926744afa3f12f7ad2dd10e0
unikaoba <89960942+unikaoba@users.noreply.github.com>
2023-01-16 20:33:13 +0800

    Add files via upload

---
+++ lib/model/counter_model.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CounterModel {
+  final String businessid;
+  final String businessName;
+  final String businessDescription;
+  final String businessType;
+  final String businessImage;
+  final String openingday;
+  final String openinghour;
+  final String closingday;
+  final String closinghour;
+  final String businessaddress;
+  final int counter;
+
+  const CounterModel(
+      {required this.businessid,
+      required this.businessName,
+      required this.businessDescription,
+      required this.businessType,
+      required this.businessImage,
+      required this.openingday,
+      required this.openinghour,
+      required this.closingday,
+      required this.closinghour,
+      required this.businessaddress,
+      required this.counter
+      });
+
+  factory CounterModel.fromSnapshot(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+
+    return CounterModel(
+        businessid: snapshot['businessid'],
+        businessName: snapshot['businessName'],
+        businessDescription: snapshot['businessDescription'],
+        businessType: snapshot['businessType'],
+        businessImage: snapshot['businessImage'],
+        openingday: snapshot['opening day'],
+        openinghour: snapshot['opening hour'],
+        closingday: snapshot['closing day'],
+        closinghour: snapshot['closing hour'],
+        businessaddress: snapshot['business address'],
+        counter: snapshot['counter']
+        );
+  }
+
+  Map<String, dynamic> toJson() => {
+        "businessid": businessid,
+        "businessName": businessName,
+        "businessDescription": businessDescription,
+        "businessType": businessType,
+        "businessImage": businessImage,
+        "opening day": openingday,
+        "opening hour": openinghour,
+        "closing day": closingday,
+        "closing hour": closinghour,
+        "business address": businessaddress,
+        "counter": counter
+      };
+}
