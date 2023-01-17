4cc3b03f479175c7b296a6684e519a7a1080d5d2
Jericho Sandoval <86719994+ekyut123@users.noreply.github.com>
2023-01-17 13:14:19 +0800

    Update rating_page.dart

--- lib/navpages/rating_page.dart
+++ lib/navpages/rating_page.dart
@@ -33,40 +33,36 @@ class _RatingPageState extends State<RatingPage> {
               body: ListView.builder(
                   itemCount: ratingcollection!.length,
                   itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(1.0),
-                      child: SizedBox(
-                        width: 150,
-                        height: 90,
-                        child: Card(
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(4.0),
-                            ),
-                            side:
-                                BorderSide(color: Colors.deepOrange, width: 3),
+                    return SizedBox(
+                      width: 150,
+                      height: 90,
+                      child: Card(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(4.0),
                           ),
-                          color: Colors.white70,
-                          child: ListTile(
-                            title: AppSemiLargeText(
-                                size: 20,
-                                text: ratingcollection[index]
-                                    .sdchosenservicename),
-                            subtitle: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                    '${ratingcollection[index].firstName} ${ratingcollection[index].lastName}',
-                                    style: const TextStyle(
-                                        color: Colors.black, fontSize: 20)),
-                                Text(
-                                  'Rating: ${ratingcollection[index].rating}',
+                          side: BorderSide(color: Colors.deepOrange, width: 3),
+                        ),
+                        color: Colors.white70,
+                        child: ListTile(
+                          title: AppSemiLargeText(
+                              size: 20,
+                              text:
+                                  ratingcollection[index].sdchosenservicename),
+                          subtitle: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                  '${ratingcollection[index].firstName} ${ratingcollection[index].lastName}',
                                   style: const TextStyle(
-                                      color: Colors.black, fontSize: 20),
-                                ),
-                              ],
-                            ),
+                                      color: Colors.black, fontSize: 20)),
+                              Text(
+                                'Rating: ${ratingcollection[index].rating}',
+                                style: const TextStyle(
+                                    color: Colors.black, fontSize: 20),
+                              ),
+                            ],
                           ),
                         ),
                       ),
