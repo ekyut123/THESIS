917ca717e6af62ee83c75d842530060725b187df
Jericho Sandoval <86719994+ekyut123@users.noreply.github.com>
2023-01-17 13:04:18 +0800

    Update rating_page.dart

--- lib/navpages/rating_page.dart
+++ lib/navpages/rating_page.dart
@@ -15,44 +15,64 @@ class _RatingPageState extends State<RatingPage> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<RatingModel>>(
-      stream: ReadDataBase.readrating(widget.businessid),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
+        stream: ReadDataBase.readrating(widget.businessid),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            print(snapshot.error);
+            return const Center(child: Text('some error occured'));
+          }
+          if (snapshot.hasData) {
+            final ratingcollection = snapshot.data;
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Service Ratings'),
+              ),
+              body: ListView.builder(
+                  itemCount: ratingcollection!.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(1.0),
+                      child: SizedBox(
+                        width: 150,
+                        height: 150,
+                        child: Card(
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(4.0)),
+                              side: BorderSide(
+                                  color: Colors.deepOrange, width: 3)),
+                          color: Colors.white70,
+                          child: ListTile(
+                            title: AppSemiLargeText(
+                                size: 20,
+                                text: ratingcollection[index]
+                                    .sdchosenservicename),
+                            subtitle: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                    '${ratingcollection[index].firstName} ${ratingcollection[index].lastName}',
+                                    style: const TextStyle(
+                                        color: Colors.black, fontSize: 20)),
+                                Text(
+                                  'Rating: ${ratingcollection[index].rating}',
+                                  style: const TextStyle(
+                                      color: Colors.black, fontSize: 20),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            );
+          }
           return const Center(child: CircularProgressIndicator());
-        }
-        if (snapshot.hasError) {
-          print(snapshot.error);
-          return const Center(child: Text('some error occured'));
-        }
-        if(snapshot.hasData){
-          final ratingcollection = snapshot.data;
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Service Ratings'),
-            ),
-            body: ListView.builder(
-              itemCount: ratingcollection!.length,
-              itemBuilder: (context, index){
-                return Card(
-                  color: Colors.white70,
-                  child: ListTile(
-                    title: AppSemiLargeText(text: ratingcollection[index].sdchosenservicename),
-                    subtitle: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('${ratingcollection[index].firstName} ${ratingcollection[index].lastName}'),
-                        Text('Rating: ${ratingcollection[index].rating}')
-                      ],
-                    ),
-                  ),
-                );
-              }
-            ),
-          );
-        }
-        return const Center(child: CircularProgressIndicator());
-      }
-    );
+        });
   }
 }
