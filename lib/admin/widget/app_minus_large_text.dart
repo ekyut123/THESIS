8b50edad7993100683a64bdad79afb2bb2748aba
unikaoba <89960942+unikaoba@users.noreply.github.com>
2023-01-18 15:13:58 +0800

    Add files via upload

---
+++ lib/admin/widget/app_minus_large_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class AppMinusLargeText extends StatelessWidget {
+  double size;
+  final String text;
+  final Color color;
+  AppMinusLargeText(
+      {Key? key,
+      this.size = 20,
+      required this.text,
+      this.color = Colors.deepOrange})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style:
+          TextStyle(color: color, fontSize: size, fontWeight: FontWeight.w800),
+    );
+  }
+}
