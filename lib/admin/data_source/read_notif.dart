bd652252cc2e3638f84e89fc1d641241ad59262c
robnnj <103233129+robnnj@users.noreply.github.com>
2023-01-26 09:58:14 +0800

    Update read_notif.dart

--- lib/admin/data_source/read_notif.dart
+++ lib/admin/data_source/read_notif.dart
@@ -3,12 +3,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../model/notif_model.dart';
 
 class ReadNotif {
-  static Stream <List<MessagesModel>> readmessages(String businessid){
+  static Stream<List<MessagesModel>> readmessages(String businessid) {
     final messagescollection = FirebaseFirestore.instance
-    .collection('Users')
-    .doc(businessid)
-    .collection('Mail');
+        .collection('Users')
+        .doc(businessid)
+        .collection('Mail')
+        .orderBy('date', descending: true);
     return messagescollection.snapshots().map((querySnapshot) =>
-      querySnapshot.docs.map((e) => MessagesModel.fromSnapshot(e)).toList());
+        querySnapshot.docs.map((e) => MessagesModel.fromSnapshot(e)).toList());
   }
 }
