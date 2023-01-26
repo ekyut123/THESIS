95018ffdc21d22215476e284fbc4c0d9a3dbf0f3
robnnj <103233129+robnnj@users.noreply.github.com>
2023-01-26 09:57:55 +0800

    Update paymentHistory_DB.dart

--- lib/admin/data_source/paymentHistory_DB.dart
+++ lib/admin/data_source/paymentHistory_DB.dart
@@ -1,14 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_firebase_users/admin/model/paymentHistory_model.dart';
 
-import '../model/notif_model.dart';
 
 class PaymentHistoryDB {
   static Stream<List<PaymentModel>> readpaymenthistory(String businessid) {
     final paymentcollection = FirebaseFirestore.instance
         .collection('Users')
         .doc(businessid)
-        .collection('Payment History');
+        .collection('Payment History')
+        .orderBy('date', descending: true);
     return paymentcollection.snapshots().map((querySnapshot) =>
         querySnapshot.docs.map((e) => PaymentModel.fromSnapshot(e)).toList());
   }
