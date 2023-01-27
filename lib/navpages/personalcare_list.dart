33df2f48b543110ead62b0d17785b068e11bc68e
robnnj <103233129+robnnj@users.noreply.github.com>
2023-01-27 20:05:39 +0800

    Update personalcare_list.dart

--- lib/navpages/personalcare_list.dart
+++ lib/navpages/personalcare_list.dart
@@ -6,67 +6,63 @@ import 'business_page.dart';
 class PersonalCareList extends StatefulWidget {
   final String label;
   final String uid;
-  const PersonalCareList({
-    super.key,
-    required this.label,
-    required this.uid
-  });
+  const PersonalCareList({super.key, required this.label, required this.uid});
 
   @override
   State<PersonalCareList> createState() => _PersonalCareListState();
 }
 
 class _PersonalCareListState extends State<PersonalCareList> {
-  
   @override
   Widget build(BuildContext context) {
-        return StreamBuilder<List<CounterModel>>(
-          stream: ReadDataBase.readbookedcounter(widget.uid, widget.label),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            if (snapshot.hasError) {
-              print(snapshot.error);
-              return const Center(child: Text('some error occured'));
-            }
-            if (snapshot.hasData) {
-              final personalcareData = snapshot.data;
-              return ListView.builder(
-                itemCount: personalcareData!.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      debugPrint(personalcareData[index].businessid);
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext context) => BusinessPage(
-                                    chosenbusiness:
-                                    personalcareData[index].businessid
-                              )
-                          )
-                      );
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.symmetric(vertical: 5),
-                      child: ListTile(
-                        //businessimage
-                        leading: CircleAvatar(
-                          radius: 30,
-                          backgroundImage:
-                            NetworkImage(personalcareData[index].businessImage),
-                        ),
-                        //businessname
-                        title: Text(personalcareData[index].businessName),
-                        subtitle: Text(personalcareData[index].businessDescription)),
-                      ),
-                    );
-                  },
-                );
-            }
+    return StreamBuilder<List<CounterModel>>(
+        stream: ReadDataBase.readbookedcounter(widget.uid, widget.label),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           }
-        );
+          if (snapshot.hasError) {
+            print(snapshot.error);
+            return const Center(child: Text('some error occured'));
+          }
+          if (snapshot.hasData) {
+            final personalcareData = snapshot.data;
+            return ListView.builder(
+              itemCount: personalcareData!.length,
+              itemBuilder: (context, index) {
+                return personalcareData[index].isSubbed
+                    ? GestureDetector(
+                        onTap: () {
+                          debugPrint(personalcareData[index].businessid);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      BusinessPage(
+                                          chosenbusiness:
+                                              personalcareData[index]
+                                                  .businessid)));
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(vertical: 5),
+                          child: ListTile(
+                              //businessimage
+                              leading: CircleAvatar(
+                                radius: 30,
+                                backgroundImage: NetworkImage(
+                                    personalcareData[index].businessImage),
+                              ),
+                              //businessname
+                              title: Text(personalcareData[index].businessName),
+                              subtitle: Text(
+                                  personalcareData[index].businessDescription)),
+                        ),
+                      )
+                    : Container();
+              },
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        });
   }
 }
