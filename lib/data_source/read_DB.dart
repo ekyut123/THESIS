da45ce7cd2e5e7cf0d88fb5775487b391b12729e
unikaoba <89960942+unikaoba@users.noreply.github.com>
2023-02-06 19:24:52 +0800

    Update read_DB.dart

--- lib/data_source/read_DB.dart
+++ lib/data_source/read_DB.dart
@@ -1,95 +1,102 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_firebase_users/navpages/all_care.dart';
-import 'package:flutter_firebase_users/navpages/healthcare_list.dart';
-import 'package:flutter_firebase_users/navpages/personalcare_list.dart';
-import 'package:flutter_firebase_users/searchdelegate.dart';
-//import 'package:thesisapp_test/widgets/app_short_text.dart';
-import 'package:flutter_firebase_users/widgets/navigation_drawer.dart';
-
+import '../model/rating_model.dart';
+import '../model/counter_model.dart';
+import '../model/booking_history.dart';
+import '../model/business_model.dart';
+import '../model/service_model.dart';
+import '../model/active_booking.dart';
+import '../model/b_bookinginfo.dart';
 //added search
-class ConsumerPage extends StatefulWidget {
-  const ConsumerPage({super.key});
-
-  @override
-  State<ConsumerPage> createState() => _ConsumerPageState();
-}
-
-final String userid = FirebaseAuth.instance.currentUser!.uid;
-final FirebaseAuth _auth = FirebaseAuth.instance;
-
-late String email;
-late String firstName;
-late String lastName;
-late String phoneNumber;
-
-class _ConsumerPageState extends State<ConsumerPage>
-    with TickerProviderStateMixin {
-  Future<DocumentSnapshot<Map<String, dynamic>>> getUser() async {
-    User? user = _auth.currentUser!;
-    return await FirebaseFirestore.instance
-        .collection('Users')
-        .doc(user.uid)
-        .get();
+class ReadDataBase {
+  static Stream<List<BusinessInfo>> readbusinessinfo() {
+    final businessinfocollection =
+        FirebaseFirestore.instance
+        .collection('BusinessList')
+        .orderBy('counter', descending: true);
+    return businessinfocollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => BusinessInfo.fromSnapshot(e)).toList());
   }
 
-  @override
-  Widget build(BuildContext context) {
-    TabController tabController = TabController(length: 3, vsync: this);
-    TextEditingController textController = TextEditingController();
-    return Scaffold(
-        drawer: const NavigationDrawerWidget(),
-        appBar: AppBar(
-          title: SizedBox(
-            height: 40.0,
-            child: Image.asset('assets/logo.png'),
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  showSearch(context: context, delegate: MySearchDelegate());
-                },
-                icon: const Icon(Icons.search))
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              //categories
-              Container(
-                alignment: Alignment.centerLeft,
-                height: 60,
-                child: Align(
-                  child: TabBar(
-                      labelColor: Colors.black,
-                      unselectedLabelColor: Colors.grey,
-                      labelPadding: const EdgeInsets.only(left: 10, right: 10),
-                      controller: tabController,
-                      indicatorSize: TabBarIndicatorSize.label,
-                      //isScrollable: true,
-                      tabs: const [
-                        Tab(text: "Personal Care"),
-                        Tab(text: "Health Care"),
-                        Tab(text: "All Care")
-                      ]),
-                ),
-              ),
-              //firstlayer]
-              Container(
-                  height: 600,
-                  width: double.maxFinite,
-                  margin: const EdgeInsets.only(left: 5),
-                  child: TabBarView(
-                    controller: tabController,
-                    children: [
-                      PersonalCareList(label: "Personal Care", uid: userid),
-                      HealthCareList(label: "Health Care", uid: userid),
-                      AllCareList(uid: userid)
-                    ],
-                  )),
-            ],
-          ),
-        ));
+  static Stream<List<BusinessInfo>> readqueryinfo(String query) {
+    final businessinfocollection = FirebaseFirestore.instance
+        .collection('BusinessList')
+        .where('bname', isGreaterThanOrEqualTo: query)
+        .where('bname', isLessThan: '${query}z');
+    return businessinfocollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => BusinessInfo.fromSnapshot(e)).toList());
+  }
+
+  static Stream<List<BusinessInfo>> readpersonalcareinfo() {
+    final personalcarecollection = FirebaseFirestore.instance
+        .collection('BusinessList')
+        .where('businessType', isEqualTo: "Personal Care");
+    return personalcarecollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => BusinessInfo.fromSnapshot(e)).toList());
+  }
+
+  static Stream<List<BusinessInfo>> readhealthcareinfo() {
+    final healthcarecollection = FirebaseFirestore.instance
+        .collection('BusinessList')
+        .where('businessType', isEqualTo: "Health Care");
+    return healthcarecollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => BusinessInfo.fromSnapshot(e)).toList());
+  }
+
+  static Stream<List<ServiceInfo>> readserviceinfo() {
+    final servicecollection = FirebaseFirestore.instance.collection('Services');
+    return servicecollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => ServiceInfo.fromSnapshot(e)).toList());
+  }
+
+  static Stream<List<BookingInfo>> readtimeslot(String id, String date) {
+    final bookingcollection = FirebaseFirestore.instance
+        .collection('BusinessList')
+        .doc(id)
+        .collection('All Bookings')
+        .doc(date)
+        .collection('Slots');
+    return bookingcollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => BookingInfo.fromSnapshot(e)).toList());
+  }
+
+  static Stream<List<ActiveBooking>> readactivebooking(String userid) {
+    final userbookingcollection = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(userid)
+        .collection('Active Booking')
+        .orderBy('date');
+    return userbookingcollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => ActiveBooking.fromSnapshot(e)).toList());
+  }
+
+  static Stream<List<BookingHistory>> readbookinghistory(String userid) {
+    final userbookinghistorycollection = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(userid)
+        .collection('Booking History')
+        .orderBy('date');
+    return userbookinghistorycollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => BookingHistory.fromSnapshot(e)).toList());
+  }
+
+  static Stream<List<RatingModel>> readrating(String businessid) {
+    final ratingcollection = FirebaseFirestore.instance
+        .collection('BusinessList')
+        .doc(businessid)
+        .collection('Ratings')
+        .orderBy('date', descending: true);
+    return ratingcollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => RatingModel.fromSnapshot(e)).toList());
+  }
+  
+  static Stream<List<CounterModel>> readbookedcounter(String userid, String label) {
+    final ratingcollection = FirebaseFirestore.instance
+    .collection('Users')
+    .doc(userid)
+    .collection('Booked Counter')
+    .where('businessType', isEqualTo: label)
+    .orderBy('counter', descending: true);
+    return ratingcollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => CounterModel.fromSnapshot(e)).toList());
   }
 }
